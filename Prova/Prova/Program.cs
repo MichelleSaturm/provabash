bf9111eea848b98c91ac77c734cd9e5cfb58e0b0
MichelleSaturm <michelle_murtas@outlook.it>
2021-06-30 16:02:48 +0200

    cambiato qualcosa

--- Prova/Prova/Program.cs
+++ Prova/Prova/Program.cs
@@ -6,7 +6,7 @@ namespace Prova
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wewe");
         }
     }
 }
